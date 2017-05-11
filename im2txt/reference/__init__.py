9b0b80c4431be86f7459ea2a1d6ecc4b36025bed
MeteorsHub <JimRanor@outlook.com>
2017-05-11 19:43:44 +0800

    create

---
+++ im2txt/reference/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+# Author: MeteorsHub
+# License: BSD Licence 
+# Contact: JimRanor@outlook.com
+# Site: http://www.meteorshub.com
+# File: __init__.py.py
+# Time: 2017/5/9 15:40
+
+'''
+    Description
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
